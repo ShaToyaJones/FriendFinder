8673a56b1cda05225a2ec1bcde2558581c6cf9f7
ShaToyaJones <jshatoyaj@yahoo.com>
2017-11-04 20:04:05 -0400

    Added dummy data into friendsArray

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,48 @@
 var friendsArray = [{
-	"name": "",
-	"photo": "",
+	"name": "Toya1",
+	"photo": "imagexyz.img",
 	"scores": [
+	"3",
+	"3",
+	"1",
+	"5",
+	"5",
+	"5",
+	"2",
+	"1",
+	"1",
+	"5"
+	]
+};
+
+	"name": "Toya2",
+	"photo": "imagexyztwo.img",
+	"scores": [
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1"
+	]
+};
+	"name": "Toya3",
+	"photo": "imagexyzthree.img",
+	"scores": [
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5"
 	]
 }];
 
